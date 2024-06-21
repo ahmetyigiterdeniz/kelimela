5ca044ba6c3e86f360f2d7098747b577a219fa0c
Erdeniz <108977954+ahmetyigiterdeniz@users.noreply.github.com>
2024-06-21 14:58:10 +0300

    Media queries (gamedisplay.tsx)
    Added media queries.

--- components/gamedisplay.tsx
+++ components/gamedisplay.tsx
@@ -24,11 +24,30 @@ const TextInput = styled.input`
     outline: none;
     height: 70px;
     width: 290px;
+    padding-left: 15px;
 
-    @media(max-width: 600px){
+    @media(max-width: 240px){
         width: 140px;
         height: 30px;
         font-size: 15px;
+    }
+
+    @media(max-width: 600px) and (min-width: 240px) {
+        width: 200px;
+        height: 50px;
+        font-size: 20px;
+    }
+
+    @media(max-width: 1000px) and (min-width: 600px) and (min-height: 700px){
+        width: 380px;
+        height: 65px;
+        font-size: 26px;
+    }
+
+    @media(min-width: 1000px) and (min-height: 800px){
+        width: 500px;
+        height: 80px;
+        font-size: 40px;
     }
 `;
 
@@ -39,18 +58,36 @@ const Button = styled.button`
     font-size: 30px;
     font-weight: 700;
     border-radius: 10px;
-    height: 70px;
-    width: 290px;
+    display: grid;
+    place-items: center;
     height: 70px;
     width: 290px;
 
-    @media(max-width: 600px){
-        padding: 8px;
-        font-size: 10px;
+    @media(max-width: 240px){
+        padding: 10px;
         width: 140px;
         height: 30px;
-        border-radius: 3px;
+        font-size: 8px;
     }
+
+    @media(max-width: 600px) and (min-width: 240px) {
+        width: 200px;
+        height: 50px;
+        font-size: 20px;
+    }
+
+    @media(max-width: 1000px) and (min-width: 600px) and (min-height: 700px){
+        width: 380px;
+        height: 65px;
+        font-size: 26px;
+    }
+
+    @media(min-width: 1000px) and (min-height: 800px){
+        width: 500px;
+        height: 80px;
+        font-size: 40px;
+    }
+
     margin-bottom: 10px;
 `;
 
@@ -70,10 +107,28 @@ const Card = styled.div`
     font-weight: 800;
     background: white;
 
-    @media(max-width: 600px) {
+    @media(max-width: 240px){
         width: 40px;
         height: 40px;
-        font-size: 20px;
+        font-size: 22px;
+    }
+
+    @media(max-width: 600px) and (min-width: 240px) {
+        width: 60px;
+        height: 60px;
+        font-size: 40px;
+    }
+
+    @media(max-width: 1000px) and (min-width: 600px) and (min-height: 700px) {
+        width: 120px;
+        height: 120px;
+        font-size: 58px;
+    }
+
+    @media(min-width: 1000px) and (min-height: 800px){
+        width: 160px;
+        height: 160px;
+        font-size: 100px;
     }
 
     animation: 1s ${fadeIn} ease-out;
@@ -87,11 +142,29 @@ const GreenCard = styled.div`
     font-size: 40px;
     font-weight: 800;
     background: green;
-
-    @media(max-width: 600px) {
+    
+    @media(max-width: 240px){
         width: 40px;
         height: 40px;
-        font-size: 20px;
+        font-size: 22px;
+    }
+
+    @media(max-width: 600px) and (min-width: 240px) {
+        width: 60px;
+        height: 60px;
+        font-size: 40px;
+    }
+
+    @media(max-width: 1000px) and (min-width: 600px) and (min-height: 700px) {
+        width: 120px;
+        height: 120px;
+        font-size: 58px;
+    }
+
+    @media(min-width: 1000px) and (min-height: 800px){
+        width: 160px;
+        height: 160px;
+        font-size: 100px;
     }
 
     animation: 1s ${fadeIn} ease-out;
@@ -106,11 +179,29 @@ const YellowCard = styled.div`
     font-size: 40px;
     font-weight: 800;
     background: yellow;
-
-    @media(max-width: 600px) {
+    
+    @media(max-width: 240px){
         width: 40px;
         height: 40px;
-        font-size: 20px;
+        font-size: 22px;
+    }
+
+    @media(max-width: 600px) and (min-width: 240px) {
+        width: 60px;
+        height: 60px;
+        font-size: 40px;
+    }
+
+    @media(max-width: 1000px) and (min-width: 600px) and (min-height: 700px){
+        width: 120px;
+        height: 120px;
+        font-size: 58px;
+    }
+
+    @media(min-width: 1000px) and (min-height: 800px){
+        width: 160px;
+        height: 160px;
+        font-size: 100px;
     }
 
     animation: 1s ${fadeIn} ease-out;
